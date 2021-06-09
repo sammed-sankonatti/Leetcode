1542ba3613bb3876242f0f7a5a43982126e90f0e
sammed-sankonatti <sammed.sankonatti45@gmail.com>
2021-06-09 16:52:17 +0530

    space optimization

--- C++/2.add_2_numbers.cpp
+++ C++/2.add_2_numbers.cpp
@@ -9,35 +9,24 @@ struct ListNode {
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
-class Solution
-{
+class Solution{
 public:
-    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
-    {
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2){
         ListNode *dummy = new ListNode(), *cur = dummy;
-
         int carry = 0;
-
-        while (l1 || l2 || carry)
-        {
-            if (l1)
-            {
+        while (l1 || l2 || carry){
+            if (l1){
                 carry += l1->val;
                 l1 = l1->next;
             }
-
-            if (l2)
-            {
+            if (l2){
                 carry += l2->val;
                 l2 = l2->next;
             }
-
             cur->next = new ListNode(carry % 10);
             carry /= 10;
-
             cur = cur->next;
         }
-
         return dummy->next;
     }
 };
